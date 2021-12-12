76b139ed1f824f0fc0dd0962ba4cb4bacf5428db
anontason <aonn@ewitewit.com>
2021-12-12 17:45:21 +0700

    nft marketplace

---
+++ src/components/Header.js
@@ -0,0 +1,40 @@
+import React from "react";
+import "./Header.css";
+import punkLogo from "../assets/header/cryptopunk-logo.png";
+import searchIcon from "../assets/header/search.png";
+import themeIcon from "../assets/header/theme-switch.png";
+
+function Header() {
+  return (
+    <div className="header">
+      <div className="logoContainer">
+        <img src={punkLogo} className="punkLogo" alt="" />
+      </div>
+
+      <div className="searchBar">
+        <div className="searchIconContainer">
+          <img src={searchIcon} alt="" />
+        </div>
+        <input
+          className="searchInput"
+          placeholder="Collection, item or user ...."
+        />
+      </div>
+
+      <div className="headerItems">
+        <p>Drops</p>
+        <p>Marketplace</p>
+        <p>Create</p>
+      </div>
+
+      <div className="headerActions">
+        <div className="headerThemeIcon">
+          <img src={themeIcon} alt="" />
+        </div>
+        <button className="loginButton">GET IN</button>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
---
+++ src/components/Main.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from "react";
+import "./Main.css";
+import instagramLogo from "../assets/owner/instagram.png";
+import twitterLogo from "../assets/owner/twitter.png";
+import more from "../assets/owner/more.png";
+
+function Main({ selectedPunk, punkListData }) {
+  const [activePunk, setActivePunk] = useState(punkListData[0]);
+
+  console.log(activePunk);
+  useEffect(() => {
+    setActivePunk(punkListData[selectedPunk]);
+  }, [punkListData, selectedPunk]);
+
+  return (
+    <div className="main">
+      <div className="mainContent">
+        <div className="punkHighlight">
+          <div className="punkContainer">
+            <img
+              className="selectedPunk"
+              src={activePunk.image_original_url}
+              alt=""
+            />
+          </div>
+        </div>
+        <div className="punkDetails" style={{ color: "#fff" }}>
+          <div className="title">
+            <div className="mainName">{activePunk.name}</div>
+            <span className="itemNumber">•#{activePunk.token_id}</span>
+          </div>
+          <div className="owner">
+            <div className="ownerImageContainer">
+              <img src={activePunk.owner.profile_img_url} alt="" />
+            </div>
+            <div className="ownerDetails">
+              <div className="ownerNameAndHandle">
+                <div>{activePunk.owner.address}</div>
+                <div className="ownerHandle">@anonDeveloper</div>
+              </div>
+              <div className="ownerLink">
+                <img src={instagramLogo} alt="" />
+              </div>
+              <div className="ownerLink">
+                <img src={twitterLogo} alt="" />
+              </div>
+              <div className="ownerLink">
+                <img src={more} alt="" />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Main;
